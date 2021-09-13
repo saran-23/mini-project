1ee9890d14ed423a0195effeac022721e8849f2a
Saran-23 <romansaran446@gmail.com>
2021-09-13 22:44:37 +0530

    Updates

---
+++ precache-manifest.fc3f76591dd58cb4eb160daab45ca796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79123e6c5fb2ee8ce065712fc7b0dead",
+    "url": "/saran-23.github.io/mini-project/index.html"
+  },
+  {
+    "revision": "61607e2658297f06321f",
+    "url": "/saran-23.github.io/mini-project/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "82fcbcb56ce3a59b69f0",
+    "url": "/saran-23.github.io/mini-project/static/js/2.4c88b277.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/saran-23.github.io/mini-project/static/js/2.4c88b277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61607e2658297f06321f",
+    "url": "/saran-23.github.io/mini-project/static/js/main.34e18dfa.chunk.js"
+  },
+  {
+    "revision": "867b8a04ae32051d66ff",
+    "url": "/saran-23.github.io/mini-project/static/js/runtime-main.24d9861c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/saran-23.github.io/mini-project/precache-manifest.fc3f76591dd58cb4eb160daab45ca796.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saran-23.github.io/mini-project/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
